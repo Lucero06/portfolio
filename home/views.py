b08db151119f491f458c21d3f713ae4f6d249321
L. Monse S.P <lmonserrat.sanchez@gmail.com>
2022-07-21 10:58:30 -0500

    Handler error al enviar email y status en template correo enviado

--- home/views.py
+++ home/views.py
@@ -4,9 +4,10 @@ from  django.views import View
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .forms import MailForm
-from django.core.mail import send_mail
+from django.core.mail import send_mail,BadHeaderError
 from django.conf import settings
 from decouple import config
+from smtplib import SMTPException
 
 class HomeView(View):
     def get(self,request):
@@ -24,29 +25,44 @@ class ContactView(HomeView):
     template_name='contact.html'
 
     def get(self, request):
-        print(request)
+        #print(request)
         form= self.form_class()
         return render(request, self.template_name, {'form':form,'number':config('CEL_NUMBER'), 'mail':config('EMAIL')})
     
     def post(self, request, *args, **kwargs):
         form=self.form_class(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            #print(form.cleaned_data)
             data = form.cleaned_data
-            send_mail(
-                'Portfolio message',
-                'Nombre: '+data['name']+'\n'
-                +'Celular: '+data['cellphone_number']+'\n'
-                +'Tel.Fijo: '+data['phone_number']+'\n\n'
-                +'Mensaje:\n'+data['body'],
-                config('EMAIL_HOST_USER'),
-                [config('SEND_EMAIL')],
-                fail_silently=False,
-            )
+            try:
+                send_mail(
+                    'Portfolio message',
+                    'Nombre: '+data['name']+'\n'
+                    +'Celular: '+data['cellphone_number']+'\n'
+                    +'Tel.Fijo: '+data['phone_number']+'\n\n'
+                    +'Mensaje:\n'+data['body'],
+                    config('EMAIL_HOST_USER'),
+                    [config('SEND_EMAIL')],
+                    fail_silently=False,
+                )
+            except SMTPException as e:
+                return render(request, self.template_name, 
+                        {'form': form, 
+                        'status':'fail',
+                        'number':config('CEL_NUMBER'), 
+                        'mail':config('EMAIL')})
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
             form=self.form_class()
-            return render(request, self.template_name, {'form': form, 'status':'success','number':config('CEL_NUMBER'), 'mail':config('EMAIL')})
+            return render(request, self.template_name, 
+                            {'form': form, 'status':'success',
+                            'number':config('CEL_NUMBER'), 
+                            'mail':config('EMAIL')})
             
-        return render(request, self.template_name, {'form': form, 'status':'false', 'number':config('CEL_NUMBER'), 'mail':config('EMAIL')})
+        return render(request, self.template_name, 
+                        {'form': form, 'status':'false', 
+                        'number':config('CEL_NUMBER'), 
+                        'mail':config('EMAIL')})
 
 #################
 class TestView(View):
