f4fec07ee30aa2ba0a45bdf6d61cd72fd3242be0
L. Monse S.P <lmonserrat.sanchez@gmail.com>
2022-08-22 15:11:55 -0500

    Cambios menores template navbar active

--- home/urls.py
+++ home/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from home.views import MorningGreetingView, HomeView, ContactView, CvView,PreviewView
 
 urlpatterns = [
-    path('', HomeView.as_view()),
-    path('contacto', ContactView.as_view()),
-    path('cv', CvView.as_view()),
-    path('vistas', PreviewView.as_view()),
+    path('', HomeView.as_view(), name='home'),
+    path('contacto', ContactView.as_view(), name='contact'),
+    path('cv', CvView.as_view(), name='cv'),
+    path('vistas', PreviewView.as_view(), name='preview'),
     #######
     path('salute', MorningGreetingView.as_view(text2='byee')),
 ]
